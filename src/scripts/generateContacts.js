28b0d630d9791f05f4c95c4124508fabddbd41cc
IhorBut737 <igor.klad@gmail.com>
2025-06-02 21:47:20 +0300

    add contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,23 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+    const existingContacts = await readContacts();
+
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+
+    const updatedContacts = [...existingContacts, ...newContacts];
+
+    await writeContacts(updatedContacts);
+
+    console.log(`Added ${number} contacts. Total: ${updatedContacts.length}`);
+  } catch (error) {
+    console.error('Error generating contacts:', error.message);
+  }
+};
 
 generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,23 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+
+    if (contacts.length === 0) {
+      console.log('No contacts. Delete nothing..');
+      return;
+    }
+
+    const removed = contacts.pop();
+    await writeContacts(contacts);
+
+    console.log('Last contact deleted:');
+    console.log(removed);
+  } catch (error) {
+    console.error('Error deleting last contact:', error.message);
+  }
+};
 
 removeLastContact();
