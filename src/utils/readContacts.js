70ba0c5b6ad4c1eb86945447b736617792f17a19
IhorBut737 <igor.klad@gmail.com>
2025-06-03 23:02:36 +0300

    fix contacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,9 +1,9 @@
 import { PATH_DB } from '../constants/contacts.js';
-import { readFile } from 'fs/promises';
+import fs from 'fs/promises';
 
 export const readContacts = async () => {
   try {
-    const data = await readFile(PATH_DB, 'utf-8');
+    const data = await fs.readFile(PATH_DB, 'utf-8');
     return JSON.parse(data);
   } catch (error) {
     console.error('Error reading contacts:', error.message);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,10 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
-import { writeFile } from 'fs/promises';
+import fs from 'fs/promises';
 
 export const writeContacts = async (updatedContacts) => {
   try {
     const data = JSON.stringify(updatedContacts, null, 2);
-    await writeFile(PATH_DB, data, 'utf-8');
+    await fs.writeFile(PATH_DB, data, 'utf-8');
   } catch (error) {
     console.error('Contact entry error:', error.message);
   }
